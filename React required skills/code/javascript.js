d92b3c26da47c98500b0ec68463f8f943d50f32c
Selim Elyahyioui <selim.elyahyioui@gmail.com>
2022-10-16 18:01:27 +0200

    push

---
+++ React required skills/code/javascript.js
@@ -0,0 +1,10 @@
+
+// * Template literals
+    // Javascript TL
+
+    let message ='Hi';
+    let firstname ='Jacob';
+    let jstl = document.getElementById('jstl');
+    jstl.innerHTML = `output: \"${message} ${firstname}\"`;
+    
+
--- React required skills/code/script.js
+++
@@ -1,8 +0,0 @@
-// 1. Template literals
-// a) Javascript TL
-
-let message ='Hi';
-let firstname ='Jacob';
-let jstl = document.getElementById('jstl');
-jstl.innerHTML = `${message} ${firstname}`;
-
